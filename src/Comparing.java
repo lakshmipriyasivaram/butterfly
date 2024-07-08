859d4ed31fafda8574fe241c240a9a94773c9676
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-07-07 20:17:14 -0500

    Test

---
+++ src/Comparing.java
@@ -0,0 +1,13 @@
+public class Comparing {
+    public static void main(String[] args) {
+        int a=5,b=10;
+        if (a==b){
+            System.out.println("both are equal");
+        }
+         else if (a>b) {
+            System.out.println("a is grater than b");
+        } else {
+            System.out.println("b is grater than a");
+        }
+    }
+}
