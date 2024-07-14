2eae188eac3ef7834775a46add1291632c32d7b9
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-07-14 17:53:54 -0500

    Test

---
+++ src/BasicArithmetic.java
@@ -0,0 +1,11 @@
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println("Sum: "+ (a+b));
+        System.out.println("Difference: "+ (a-b));
+        System.out.println("Product: "+ (a*b));
+        System.out.println("Quotient: "+ (a/b));
+
+    }
+}
