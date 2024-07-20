804c31e2c74b071db6a01a1ceca389f475e64a45
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-07-19 22:24:18 -0500

    Test

---
+++ src/Countingnumbers.java
@@ -0,0 +1,10 @@
+public class Countingnumbers {
+    public static void main(String[] args) {
+        int count =0;
+        while(count <=15)
+        {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
