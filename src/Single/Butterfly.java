31605fb932ea480f67c0661870e7f207d75df059
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-07-30 10:45:25 -0500

    Test

---
+++ src/Single/Butterfly.java
@@ -0,0 +1,14 @@
+package Single;
+
+public class Butterfly extends Insects {
+        public int wings =2;
+        public static void main(String[] args)
+        {
+            Butterfly butterfly = new Butterfly();
+
+            System.out.println(butterfly.wings);
+            System.out.println(butterfly.size);
+        }
+
+}
+
---
+++ src/Single/Insects.java
@@ -0,0 +1,11 @@
+package Single;
+
+public class Insects {
+    protected int no_of_wings = 2;
+
+    protected String size = "small";
+    protected void colour()
+    {
+        System.out.println("blue");
+    }
+}
